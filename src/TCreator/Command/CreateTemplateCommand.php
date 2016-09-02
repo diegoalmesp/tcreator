bd17506c5691523409cd60596f7a61e4f6572756
Diego <diegoalmesp@gmail.com>
2016-09-02 11:47:11 -0300

    added Filesystem Component

--- src/TCreator/Command/CreateTemplateCommand.php
+++ src/TCreator/Command/CreateTemplateCommand.php
@@ -6,6 +6,11 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+// file system
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+use Templates\Standard\Index as Content;
 
 class CreateTemplateCommand extends Command
 {
@@ -22,6 +27,8 @@ class CreateTemplateCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $contents = Content::getText();
+        // var_dump($contents);
         $output->writeln([
             '',
             'TCreator app',
@@ -31,6 +38,17 @@ class CreateTemplateCommand extends Command
         ]);
 
         $output->writeln('File Name: ' . $input->getArgument('filename'));
+
+        $fs = new Filesystem();
+
+        try {
+            // $fs->mkdir($input->getArgument('filename').'.php');
+            // $fs->touch($input->getArgument('filename').'.php');
+            $fs->dumpFile($input->getArgument('filename').'.php', $contents);
+
+        } catch (Exception $e) {
+            echo "An error occurred while creating your directory at ".$e->getPath();
+        }
     }
 }
 
