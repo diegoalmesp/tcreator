a552f4afaebade9eb62266d68b4051b50d937f74
Diego <diegoalmesp@gmail.com>
2016-09-05 10:45:41 -0300

    readme updated

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 ## run
     > php bin/tcreator
+
+## create a new template
+
+    > php bin\tcreator template:create myNewFolder
+
+Here "myNewFolder" (required) is the new directory that will be created:
+
+    /myNewFolder
+    |-- css
+    |   `-- target.css
+    `-- index.php
+
+**index.php** contains the contents of the files inside /public folder. TCreator will **only** include the files that start with a number
---
+++ newFolder/index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>Bienvenidos - Mi Claro</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Claro">
+    <link rel="stylesheet" href="http://madariaga2.claro.amx:8008/HTML(Nueva%20libreria)/css/bootstrap.css">
+    <link rel="stylesheet" href="http://madariaga2.claro.amx:8008/HTML(Nueva%20libreria)/css/header-footer-nuevo.css">
+    <link rel="stylesheet" href="http://madariaga2.claro.amx:8008/HTML(Nueva%20libreria)/css/custom2.css">
+    <link rel="stylesheet" href="http://madariaga2.claro.amx:8008/HTML(Nueva%20libreria)/css/general.css">
+    <link rel="stylesheet" href="http://madariaga2.claro.amx:8008/HTML(Nueva%20libreria)/css/libreria.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/target.css">
+    <!-- /Siempre incluimos esto-->
+    <!-- Momentaneo-->
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <!-- Web Font de Iconos-->
+    <link rel="stylesheet" href="fonts/font.css">
+    <link rel="stylesheet" href="fonts/font-awesome.min.css">
+    <!-- //// -->
+    <!-- Fuente Roboto-->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- //// -->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body style="background-color: #eeeeee;">
+
+<div class="login-flex" id="page" style="background-image: url(../../img/loginBackgroundAR.jpg);">
+    <!-- Barra Negra-->
+   <div class="header-superior">
+      <div class="container">
+         <ul class="barra-flex">
+            <li>
+               <a href="#" role="button" class="icono-persona-link txt-13 txt-300"></i>Mis Datos</a>
+            </li>
+            <li>
+               <a href="#" role="button" class="icono-sesion-link txt-13 txt-300"></i>Cerrar Sesión</a>
+            </li>
+         </ul>
+      </div>
+    </div>   <!-- //// -->
+
+      <!-- Barra Roja-->
+     <nav class="navbar navbar-default navbar-fixed-top navbar-roja">
+       <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+             <span class="sr-only">Desplegar menu</span>
+             <i class="fa fa-bars" aria-hidden="true"></i>
+             <i class="fa fa-times" aria-hidden="true"></i>
+             </button>     
+             <a class="navbar-brand" href="index.html"><img alt="MiClaro" src="img/logoClaro.svg" class="img-responsive"></a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="navbar">
+             <ul class="nav navbar-nav">
+                <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Consultas <span class=" hidden-xs"><i class="fa fa-angle-down arrow-down"></i></span>
+                      <div class="deplegar-animado visible-xs">
+                         <i class="fa fa-minus" aria-hidden="true"></i>
+                         <i class="fa fa-plus" aria-hidden="true"></i>
+                      </div>
+                   </a>
+                   <ul class="dropdown-menu animated slideDown">
+                      <li><a href="#">Mi Consumo</a></li>
+                      <li><a href="#">Detalles de Consumos</a></li>
+                      <li><a href="#">Mi Plan</a></li>
+                      <li><a href="#">Servicio Técnico</a></li>
+                      <li><a href="#">Envío por Delivery</a></li>
+                   </ul>
+                </li>
+                <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Servicios <span class=" hidden-xs"><i class="fa fa-angle-down arrow-down"></i></span>
+                      <div class="deplegar-animado visible-xs">
+                         <i class="fa fa-minus" aria-hidden="true"></i>
+                         <i class="fa fa-plus" aria-hidden="true"></i>
+                      </div>
+                   </a>
+                   <ul class="dropdown-menu animated slideDown">
+                      <li><a href="#">Gestionar mis Packs</a></li>
+                      <li><a href="#">Mis Suscripciones</a></li>
+                      <li><a href="#">Recarga de Crédito</a></li>
+                      <li><a href="#">Configurar mi Línea</a></li>
+                      <li><a href="#">Configurar mi Equipo</a></li>
+                   </ul>
+                </li>
+                <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Mi Cuenta <span class=" hidden-xs"><i class="fa fa-angle-down arrow-down"></i></span>
+                      <div class="deplegar-animado visible-xs">
+                         <i class="fa fa-minus" aria-hidden="true"></i>
+                         <i class="fa fa-plus" aria-hidden="true"></i>
+                      </div>
+                   </a>
+                   <ul class="dropdown-menu  animated slideDown">
+                      <li><a href="#">Mi Factura</a></li>
+                      <li><a href="#">Administrar mi Factura Electrónica</a></li>
+                      <li><a href="#">Informar un Pago</a></li>
+                      <li><a href="#">Pagar mi Factura</a></li>
+                      <li><a href="#">Lugares y Formas de Pago</a></li>
+                   </ul>
+                </li>
+                <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                      Beneficios <span class=" hidden-xs"><i class="fa fa-angle-down arrow-down"></i></span>
+                      <div class="deplegar-animado visible-xs">
+                         <i class="fa fa-minus" aria-hidden="true"></i>
+                         <i class="fa fa-plus" aria-hidden="true"></i>
+                      </div>
+                   </a>
+                   <ul class="dropdown-menu  animated slideDown">
+                      <li><a href="#">Claro club</a></li>
+                   </ul>
+                </li>
+             </ul>
+             <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                   <a href="#" class="dropdown-toggle ayuda-boton" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ayuda</a>
+                   <ul class="dropdown-menu animated slideDown">
+                      <li><a href="#">Envianos tu consulta</a></li>
+                      <li><a href="#">Chat Claro</a></li>
+                   </ul>
+                </li>
+             </ul>
+          </div>
+          <!-- /.navbar-collapse -->
+       </div>
+       <!-- /.container-fluid -->
+    </nav>   <!-- //// -->
+  <!-- EL CÓDIGO VA AQUÍ -->
+  <!-- EL CÓDIGO VA AQUÍ -->
+  <!-- EL CÓDIGO VA AQUÍ -->
+
+  <footer>
+    <div class="container">
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div class="legal">
+             <a href="index.html">
+                <img alt="MiClaro" src="vector/logo-claro-blanco.svg" class="logo-claro center-block">
+                <div class="block-5 display-xs"></div>
+             </a>
+             <p class="txt-blanco txt-derechos txt-300 txt-14">Todos los derechos reservados, Claro 2016</p>
+          </div>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div class="block-5 display-xs"></div>
+          <ul class="navbar-right">
+             <li>
+                <a href="#" class="txt-300 txt-14">Institucional</a>
+             </li>
+             <li>
+                <a href="#" class="txt-300 txt-14">Legal y Regulatorio</a>
+             </li>
+          </ul>
+      </div>
+    </div>
+  </footer>
+
+</div>
+
+    <!-- //// -->
+    <!-- Footer-->
+    <!-- //// -->
+    <script src="js/vendor/jquery-1.11.1.min.js"></script>
+    <script src="js/vendor/bootstrap.js"></script>    
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="js/easyPieChart.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/rueditas.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="js/jquery.easypiechart.js"></script>
+    <script type="text/javascript">
+      $(function () {
+         
+      });
+    </script>
+</body>
+</html>
